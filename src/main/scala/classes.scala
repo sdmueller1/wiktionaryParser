f3220f84212eb9b8d908dd0b975823a5e244d1b6
Stefan Mueller <s.mueller0311@gmail.com>
2023-06-19 22:19:15 +0300

    Added data structure class

--- src/main/scala/classes.scala
+++ src/main/scala/classes.scala
@@ -1,3 +1,30 @@
+/**
+ * Simple data tree structure to place the parsed data
+ * @param name
+ * Title of the given branch
+ */
+class Datatree(val name:String):
+  //class Branch(val name:String, val depth:Int):
+  //  val branches:Array[Branch] = Array()
+  var children:Array[Datatree] = Array()
+  //def addBranches(a:Array[Datatree]):Unit = children ++= a
+  //def branches:Array[Datatree] = children
+
+  var data:Array[Any] = Array()
+  //def addData(a:Any):Unit = data :+= a
+  //def getData:Array[Any] = data
+
+  override def toString:String =
+    toString(0)
+
+  private def toString(tbs: Int): String =
+    var ret = "-"*15+"\n"+name+"\n"+"-"*15+"\n"
+    for datum <- data do
+      ret = ret + datum.toString + "\n"
+    for child <- children do
+      ret += child.toString(tbs+1) + "\n"
+    ret.replaceAll("(?m)^", "\t" * tbs)
+
 
 /**
  * Simple class representing a word in a multilingual dictionary
@@ -23,18 +50,11 @@ class Word(val s:String, val lg:String = "en"):
 class Definition(val text:String,val part:String,val ex:Option[String]=None)
 
 /**
- * A simpler version of definitions suitable for translations.
- * @param text
- * Contents of the definition
- */
-class Context(text:String)
-
-/**
  * Helper class for Word, represents a translation from one language to another.
- * @param origDef
- * What does this word mean in this context?
+ * @param context
+ * A simpler version of definitions suitable for translations.
  * @param target
  * Which word does the definition point to?
  */
-class Translation(val origDef:Context, val target:Word)
+class Translation(val context:String, val target:Word)
 
